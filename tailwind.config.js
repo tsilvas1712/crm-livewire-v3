8d6c6a0db8e57bfdd11de13927f7a13dac4db341
Thiago Silva <adsl.thiago@gmail.com>
2024-06-11 22:25:09 -0300

    crm-1: Mary UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+import forms from "@tailwindcss/forms";
+import typography from "@tailwindcss/typography";
 export default {
     content: [
         "./resources/**/*.blade.php",
         "./resources/**/*.js",
         "./resources/**/*.vue",
+        "./vendor/robsontenorio/mary/src/View/Components/**/*.php",
     ],
     theme: {
         extend: {},
     },
-    plugins: [],
+    plugins: [require("daisyui"), forms, typography],
 };
