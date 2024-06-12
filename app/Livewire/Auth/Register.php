9f3c0f0857e6c9756e25686b860d4c8336774574
Thiago Silva <adsl.thiago@gmail.com>
2024-06-11 23:58:31 -0300

    crm-1: Test Register User

--- app/Livewire/Auth/Register.php
+++ app/Livewire/Auth/Register.php
@@ -3,17 +3,21 @@
 namespace App\Livewire\Auth;
 
 use App\Models\User;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
 
 class Register extends Component
 {
+    #[Rule(["required", "max:255"])]
     public string $name;
 
+    #[Rule(["required", "email", "max:255"])]
     public string $email;
 
+    #[Rule("required")]
     public string $password;
 
-    public string $confirmed_email;
+    public ?string $confirmed_email;
 
     public function render()
     {
@@ -22,6 +26,8 @@ class Register extends Component
 
     public function submit()
     {
+        $this->validate();
+
         User::create([
             'name'     => $this->name,
             'email'    => $this->email,
--- tests/Feature/Livewire/Auth/RegisterTest.php
+++ tests/Feature/Livewire/Auth/RegisterTest.php
@@ -3,10 +3,7 @@
 use App\Livewire\Auth\Register;
 use Livewire\Livewire;
 
-it('should render the component', function () {
-    Livewire::test(Register::class)
-        ->assertOk();
-});
+use function Pest\Laravel\{assertDatabaseCount, assertDatabaseHas};
 
 it('should be able to register a new user in the system', function () {
     Livewire::test(Register::class)
@@ -16,4 +13,25 @@ it('should be able to register a new user in the system', function () {
         ->set('password', 'password')
         ->call('submit')
         ->assertHasNoErrors();
+
+    assertDatabaseHas('users', [
+        'name'  => 'John Doe',
+        'email' => 'jonh@doe.com',
+    ]);
+
+    assertDatabaseCount('users', 1);
 });
+
+test('Validation Rules', function ($f) {
+    Livewire::test(Register::class)
+        ->set($f->field, $f->value)
+        ->call('submit')
+        ->assertHasErrors([$f->field => $f->rule]);
+})->with([
+    'name::required'     => (object) ['field' => 'name', 'value' => '', 'rule' => 'required'],
+    'name::max:255'      => (object) ['field' => 'name', 'value' => str_repeat('*', 256), 'rule' => 'max:255'],
+    'email::required'    => (object) ['field' => 'email', 'value' => '', 'rule' => 'required'],
+    'email::email'       => (object) ['field' => 'email', 'value' => 'no-an-email', 'rule' => 'email'],
+    'email::max:255'     => (object) ['field' => 'email', 'value' => str_repeat('*', 256), 'rule' => 'max:255'],
+    'password::required' => (object) ['field' => 'password', 'value' => '', 'rule' => 'required'],
+]);
